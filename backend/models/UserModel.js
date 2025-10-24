33e12a7ff25cce242260efdf84e2580212cb3c95
danushka <danmadushzxc@gmail.com>
2025-10-24 21:40:20 +0530

    refactor:added designation field

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,12 +1,15 @@
+const { string, required } = require('joi');
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
-    lastName:  { type: String, required: true },
+    lastName:  { type: String },
     email: { type: String, required: true, unique:true, lowercase: true },
+    password: { type: String, required: true },
     epfNo: { type: String, required: true },
-    branchId: { type: String, required: true },
-    departmentId: { type: String, required: true },
+    branch: { type: String, required: true },
+    department: { type: String, required: true },
+    designation: { type: String, required: true },
     contactNo: { type: String, required: true, unique: true },
     isActive: { type: Boolean, required: true, default: true },
     createdBy: { type: String,  require: true },
