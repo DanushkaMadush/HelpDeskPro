e0f77579f79eff7b9b31c8b31b551d469a5fcf17
danushka <danmadushzxc@gmail.com>
2025-10-25 22:23:41 +0530

    feat:implemented endpoints for user services

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const { registerUser, signinUser } = require('../services/authService');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// User Registre
+router.post('/register', async (req, res) => {
+  try{
+    const user = await registerUser(req.body);
+    res.status(201).json({ success: true, user });
+  } catch (err) {
+    res.status(400).json({ success: false, message: err.message });
+  }
+});
+
+// User signin
+router.post('/sign-in', async (req, res) => {
+  try{
+    const result = await signinUser(req.body);
+    res.json({ success: true, ...result });
+  } catch (err) {
+    res.status(401).json({ success: false, message: err.message });
+  }
 });
 
 module.exports = router;
