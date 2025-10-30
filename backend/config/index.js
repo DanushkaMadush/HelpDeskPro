c585d356406e8ed541d04743a9ac85fa4c7a7fd4
danushka <danmadushzxc@gmail.com>
2025-10-30 20:12:41 +0530

    removed files

--- backend/config/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    PORT: process.env.PORT,
-    MONGO_URI: process.env.MONGO_URI,
-    JWT_SECRET: process.env.JWT_SECRET,
-    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
-    SALT_ROUNDS: parseInt(process.env.SALT_ROUNDS)
-};
