1af5ef9b834523a1c5bf86a977efc045ff07ab23
danushka <danmadushzxc@gmail.com>
2025-11-05 20:04:42 +0530

    feat:added dbcontext and model builder

---
+++ backend/Data/HelpDeskProDBContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using backend.Models.Entities;
+
+namespace backend.Data;
+
+public partial class HelpDeskProDBContext : DbContext
+{
+    public HelpDeskProDBContext(DbContextOptions<HelpDeskProDBContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__Users__1788CC4CB8842EDA");
+
+            entity.HasIndex(e => e.Email, "UQ__Users__A9D1053413D0032A").IsUnique();
+
+            entity.Property(e => e.ContactNo).HasMaxLength(20);
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Department).HasMaxLength(100);
+            entity.Property(e => e.Designation).HasMaxLength(100);
+            entity.Property(e => e.Email).HasMaxLength(150);
+            entity.Property(e => e.FirstName).HasMaxLength(100);
+            entity.Property(e => e.IsActive).HasDefaultValue(true);
+            entity.Property(e => e.LastName).HasMaxLength(100);
+            entity.Property(e => e.Plant).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
