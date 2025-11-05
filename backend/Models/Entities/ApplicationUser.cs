7770981148d77623fff5b8e62ef6b52fd388f4a6
danushka <danmadushzxc@gmail.com>
2025-11-05 22:07:59 +0530

    feat:added application user class

---
+++ backend/Models/Entities/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace backend.Models.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string ContactNo { get; set; } = string.Empty;
+        public string Plant { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+        public string Designation { get; set; } = string.Empty;
+    }
+}
