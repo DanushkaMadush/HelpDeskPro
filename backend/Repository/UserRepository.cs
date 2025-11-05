f5d01d7174916b8408031ab4f7108789a9958442
danushka <danmadushzxc@gmail.com>
2025-11-05 22:08:25 +0530

    feat:added repository service for user services

---
+++ backend/Repository/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using backend.Interface;
+using backend.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace backend.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserRepository(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<ApplicationUser?> GetByEmailAsync(string email)
+        {
+            return await _userManager.FindByEmailAsync(email);
+        }
+        public async Task<bool> CreateUserAsync(ApplicationUser user, string password)
+        {
+            var result = await _userManager.CreateAsync(user, password);
+            return result.Succeeded;
+        }
+    }
+}
