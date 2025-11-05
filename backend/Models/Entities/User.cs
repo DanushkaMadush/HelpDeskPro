19b8d547a52071e5730bf4d304853380ae372780
danushka <danmadushzxc@gmail.com>
2025-11-05 20:05:16 +0530

    feat:added users model using scaffold

---
+++ backend/Models/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace backend.Models.Entities;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string ContactNo { get; set; } = null!;
+
+    public string? Plant { get; set; }
+
+    public string? Department { get; set; }
+
+    public string? Designation { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public bool? IsActive { get; set; }
+}
