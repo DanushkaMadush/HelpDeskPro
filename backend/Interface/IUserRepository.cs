b487d91f586c42171c4a1c4f61f2abe5cda246fc
danushka <danmadushzxc@gmail.com>
2025-11-05 22:06:05 +0530

    feat:added user interfaces

---
+++ backend/Interface/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using backend.Models.Entities;
+using System.Threading.Tasks;
+
+namespace backend.Interface
+{
+    public interface IUserRepository
+    {
+        Task<ApplicationUser?> GetByEmailAsync(string email);
+        Task<bool> CreateUserAsync(ApplicationUser user, string password);
+    }
+}
---
+++ backend/Interface/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using backend.Models.Entities;
+
+namespace backend.Interface
+{
+    public interface IUserService
+    {
+        Task<bool> RegisterAsync(ApplicationUser user, string password);
+        Task<string?> LoginAsync(string email, string password);
+    }
+}
