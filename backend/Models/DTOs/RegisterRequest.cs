7fb4b8d1eb776274fb00f97192f58c61e3a5059c
danushka <danmadushzxc@gmail.com>
2025-11-05 22:06:42 +0530

    feat:added register and login DTOs

---
+++ backend/Models/DTOs/RegisterRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace backend.Models.DTOs
+{
+    public class RegisterRequest
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string ContactNo { get; set; } = string.Empty;
+        public string Plant { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+        public string Designation { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
