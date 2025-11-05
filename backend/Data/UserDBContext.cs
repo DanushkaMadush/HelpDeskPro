621e0d0d2afe1a8f2e62322e341b3df9edceb002
danushka <danmadushzxc@gmail.com>
2025-11-05 22:05:36 +0530

    feat: added databse context for auth services

---
+++ backend/Data/UserDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using backend.Models.Entities;
+
+namespace backend.Data
+{
+    public class UserDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public UserDBContext(DbContextOptions<UserDBContext> options)
+            : base(options)
+        {
+        }
+    }
+}
