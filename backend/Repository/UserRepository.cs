b8d4a808b40e423b03a7686026a1490c5a7551ec
danushka <danmadushzxc@gmail.com>
2025-11-06 20:48:08 +0530

    feat:added repository to create and assign roles

--- backend/Repository/UserRepository.cs
+++ backend/Repository/UserRepository.cs
@@ -7,20 +7,42 @@ namespace backend.Repository
     public class UserRepository : IUserRepository
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public UserRepository(UserManager<ApplicationUser> userManager)
+        public UserRepository(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         public async Task<ApplicationUser?> GetByEmailAsync(string email)
         {
             return await _userManager.FindByEmailAsync(email);
         }
+
         public async Task<bool> CreateUserAsync(ApplicationUser user, string password)
         {
             var result = await _userManager.CreateAsync(user, password);
             return result.Succeeded;
         }
+
+        public async Task<bool> CreateRoleAsync(string roleName)
+        {
+            if (await _roleManager.RoleExistsAsync(roleName))
+                return true;
+
+            var result = await _roleManager.CreateAsync(new IdentityRole(roleName));
+            return result.Succeeded;
+        }
+
+        public async Task<bool> AssignRoleAsync(ApplicationUser user, string roleName)
+        {
+            if (!await _roleManager.RoleExistsAsync(roleName))
+                return false;
+
+            var result = await _userManager.AddToRoleAsync(user, roleName);
+            return result.Succeeded;
+        }
+
     }
 }
