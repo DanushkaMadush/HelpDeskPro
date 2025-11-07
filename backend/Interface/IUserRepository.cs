00ff8636d2e0a199bd0c845ec8785e19aee4592b
danushka <danmadushzxc@gmail.com>
2025-11-07 21:01:21 +0530

    feat:added user repository and service interfaces

--- backend/Interface/IUserRepository.cs
+++ backend/Interface/IUserRepository.cs
@@ -9,6 +9,10 @@ namespace backend.Interface
         Task<bool> CreateUserAsync(ApplicationUser user, string password);
         Task<bool> CreateRoleAsync(string roleName);
         Task<bool> AssignRoleAsync(ApplicationUser user, string roleName);
-
+        Task<bool> CreatePermissionAsync(string name, string description);
+        Task<bool> AssignPermissionToRoleAsync(string roleName, string permissionName);
+        Task<List<string>> GetPermissionsByUserAsync(ApplicationUser user);
+        Task<bool> AssignPermissionToUserAsync(string email, string permissionName);
+        Task<List<string>> GetPermissionsByUserDirectAsync(ApplicationUser user);
     }
 }
--- backend/Interface/IUserService.cs
+++ backend/Interface/IUserService.cs
@@ -8,6 +8,8 @@ namespace backend.Interface
         Task<string?> LoginAsync(string email, string password);
         Task<bool> CreateRoleAsync(string roleName);
         Task<bool> AssignRoleAsync(string email, string roleName);
-
+        Task<bool> CreatePermissionAsync(string name, string description);
+        Task<bool> AssignPermissionToRoleAsync(string roleName, string permissionName);
+        Task<bool> AssignPermissionToUserAsync(string email, string permissionName);
     }
 }
