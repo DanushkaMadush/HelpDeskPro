3daf2d20820b87abe7e0b25b987d62912089e47e
danushka <danmadushzxc@gmail.com>
2026-04-10 08:29:11 +0530

    refactor:added dashboard and admin routes

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
+import AdminHome from "../pages/AdminHome";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/admin" element={<AdminHome />} />
       </Routes>
     </BrowserRouter>
   );
