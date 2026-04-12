7a25f83a3dc46ea140f0b3ce44dca50fbc069ef7
danushka <danmadushzxc@gmail.com>
2026-04-12 09:58:06 +0530

    feat:added admin child routes'

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
-import AdminHome from "../pages/AdminHome";
+import AdminLayout from "../pages/AdminLayout";
+import UsersPage from "../pages/admin/UsersPage";
 
 export default function AppRouter() {
   return (
@@ -9,7 +10,9 @@ export default function AppRouter() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/admin" element={<AdminHome />} />
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="users" element={<UsersPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
