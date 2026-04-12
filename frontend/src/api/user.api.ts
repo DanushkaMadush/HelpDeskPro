2949f694a5e1c97421a4c09b1dbb1c80caff6c7b
danushka <danmadushzxc@gmail.com>
2026-04-12 09:56:32 +0530

    fix:fixed return type

--- frontend/src/api/user.api.ts
+++ frontend/src/api/user.api.ts
@@ -8,9 +8,9 @@ export interface GetAllUsersResponse {
     roles?: string[];
 }
 
-export const getAllUsers = async (role: string | null): Promise<GetAllUsersResponse> => {
+export const getAllUsers = async (role: string | null): Promise<GetAllUsersResponse[]> => {
     try {
-        const res = await apiClient.get<GetAllUsersResponse>(role ? `/users?role=${role}` : '/users');
+        const res = await apiClient.get<GetAllUsersResponse[]>(role ? `/users?role=${role}` : '/users');
         return res.data;
     } catch (error: any) {
         console.error('User retrieve error', error);
