192eee8885f16c577b3fdd981710d1df537395e3
danushka <danmadushzxc@gmail.com>
2026-04-18 09:19:35 +0530

    feat:added report service interface

---
+++ backend/Interface/IReportService.cs
@@ -0,0 +1,11 @@
+﻿using backend.Models.DTOs;
+
+namespace backend.Interface
+{
+    public interface IReportService
+    {
+        Task<IEnumerable<ReportDTOs.MonthlyTicketResponse>> GetMonthlyTicketsAsync(int year);
+        Task<IEnumerable<ReportDTOs.BranchTicketResponse>> GetBranchWiseTicketsAsync();
+        Task<IEnumerable<ReportDTOs.PendingSystemResponse>> GetPendingSystemsAsync();
+    }
+}
