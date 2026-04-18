d80dfe49f22377c52101557d0c2aa07211b4f218
danushka <danmadushzxc@gmail.com>
2026-04-18 09:18:48 +0530

    feat:added report DTOs

---
+++ backend/Models/DTOs/ReportDTOs.cs
@@ -0,0 +1,26 @@
+﻿namespace backend.Models.DTOs
+{
+    public class ReportDTOs
+    {
+        public class MonthlyTicketResponse
+        {
+            public int MonthNumber { get; set; }
+            public string MonthName { get; set; } = null!;
+            public int TotalTickets { get; set; }
+        }
+
+        public class BranchTicketResponse
+        {
+            public int BranchId { get; set; }
+            public string BranchName { get; set; } = null!;
+            public int TotalTickets { get; set; }
+        }
+
+        public class PendingSystemResponse
+        {
+            public int SystemId { get; set; }
+            public string SystemName { get; set; } = null!;
+            public int PendingTickets { get; set; }
+        }
+    }
+}
