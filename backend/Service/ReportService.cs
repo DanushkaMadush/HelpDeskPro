e4dab6214af14524c8da2e5c0b7d649233f579da
danushka <danmadushzxc@gmail.com>
2026-04-18 09:20:23 +0530

    feat:added report services

---
+++ backend/Service/ReportService.cs
@@ -0,0 +1,30 @@
+﻿using backend.Interface;
+using backend.Models.DTOs;
+
+namespace backend.Service
+{
+    public class ReportService : IReportService
+    {
+        private readonly IReportRepository _repository;
+
+        public ReportService(IReportRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<IEnumerable<ReportDTOs.MonthlyTicketResponse>> GetMonthlyTicketsAsync(int year)
+        {
+            return _repository.GetMonthlyTicketsAsync(year);
+        }
+
+        public Task<IEnumerable<ReportDTOs.BranchTicketResponse>> GetBranchWiseTicketsAsync()
+        {
+            return _repository.GetBranchWiseTicketsAsync();
+        }
+
+        public Task<IEnumerable<ReportDTOs.PendingSystemResponse>> GetPendingSystemsAsync()
+        {
+            return _repository.GetPendingSystemsAsync();
+        }
+    }
+}
